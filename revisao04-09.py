5e1b261606ceedbd946267962b443aabfdb0dfff
Gabriela Martins <www.gabmartins@gmail.com>
2025-09-04 21:48:22 -0300

    Exercícios Python – Desafios 1, 2 e 3

---
+++ revisao04-09.py
@@ -0,0 +1,27 @@
+# name = str(input("Qual seu nome? "))
+# idade= int(input("Idade? "))
+# saldo = float(input("Saldo bancário? "))
+
+# print(f"Olá {name}, você tem {idade} anos e seu saldo bancário atualmente é de R$ {saldo:.2f}")
+
+# if saldo >= 1000:
+#     print("Boa reserva")
+# elif saldo >= 100:
+#     print("Cuidado , saldo razoável")
+# else:
+#     print("Alerta! Saldo muito baixo!")
+
+# Imagine que uma pessoa decidiu guardar R$50 por mês durante 12 meses.
+# O objetivo é mostrar quanto ela terá acumulado ao final de cada mês.
+
+for mes in range(1, 13):
+    saldo = mes*50
+    print(f"Mês {mes} = R$ {saldo}")
+
+mes = 1
+saldo = 0
+
+while mes < 13:
+    saldo += 50
+    print(f"Mês {mes}, saldo R$ {saldo}")
+    mes += 1
